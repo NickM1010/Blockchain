075821b9f48e67066f9a40fae264b9ad38e6f7d8
Nick <nickm10@hotmail.de>
2019-04-10 23:57:08 +0200

    Initial commit

---
+++ Blockchain/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Blockchain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTransaction = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNewblock = new System.Windows.Forms.Button();
+            this.txtBlockchain = new System.Windows.Forms.TextBox();
+            this.txtBlockcount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.transcationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.transcationhashDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.currenthashDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.previoushashDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.blockcountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dbBlockchainBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dbBlockchainDataSet1 = new Blockchain.dbBlockchainDataSet1();
+            this.dbBlockchainTableAdapter = new Blockchain.dbBlockchainDataSet1TableAdapters.dbBlockchainTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbBlockchainBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbBlockchainDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTransaction
+            // 
+            this.txtTransaction.Location = new System.Drawing.Point(753, 67);
+            this.txtTransaction.Multiline = true;
+            this.txtTransaction.Name = "txtTransaction";
+            this.txtTransaction.Size = new System.Drawing.Size(412, 190);
+            this.txtTransaction.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Existing Blockchain";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(753, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Create TX";
+            // 
+            // btnNewblock
+            // 
+            this.btnNewblock.Location = new System.Drawing.Point(756, 263);
+            this.btnNewblock.Name = "btnNewblock";
+            this.btnNewblock.Size = new System.Drawing.Size(159, 54);
+            this.btnNewblock.TabIndex = 4;
+            this.btnNewblock.Text = "Create new TX";
+            this.btnNewblock.UseVisualStyleBackColor = true;
+            this.btnNewblock.Click += new System.EventHandler(this.btnNewblock_Click);
+            // 
+            // txtBlockchain
+            // 
+            this.txtBlockchain.Location = new System.Drawing.Point(16, 237);
+            this.txtBlockchain.Multiline = true;
+            this.txtBlockchain.Name = "txtBlockchain";
+            this.txtBlockchain.ReadOnly = true;
+            this.txtBlockchain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBlockchain.Size = new System.Drawing.Size(694, 411);
+            this.txtBlockchain.TabIndex = 6;
+            // 
+            // txtBlockcount
+            // 
+            this.txtBlockcount.Location = new System.Drawing.Point(921, 282);
+            this.txtBlockcount.Name = "txtBlockcount";
+            this.txtBlockcount.ReadOnly = true;
+            this.txtBlockcount.Size = new System.Drawing.Size(100, 20);
+            this.txtBlockcount.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(921, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "BlockCount";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.transcationDataGridViewTextBoxColumn,
+            this.transcationhashDataGridViewTextBoxColumn,
+            this.currenthashDataGridViewTextBoxColumn,
+            this.previoushashDataGridViewTextBoxColumn,
+            this.blockcountDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.dbBlockchainBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(660, 150);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // transcationDataGridViewTextBoxColumn
+            // 
+            this.transcationDataGridViewTextBoxColumn.DataPropertyName = "transcation";
+            this.transcationDataGridViewTextBoxColumn.HeaderText = "transcation";
+            this.transcationDataGridViewTextBoxColumn.Name = "transcationDataGridViewTextBoxColumn";
+            // 
+            // transcationhashDataGridViewTextBoxColumn
+            // 
+            this.transcationhashDataGridViewTextBoxColumn.DataPropertyName = "transcationhash";
+            this.transcationhashDataGridViewTextBoxColumn.HeaderText = "transcationhash";
+            this.transcationhashDataGridViewTextBoxColumn.Name = "transcationhashDataGridViewTextBoxColumn";
+            // 
+            // currenthashDataGridViewTextBoxColumn
+            // 
+            this.currenthashDataGridViewTextBoxColumn.DataPropertyName = "currenthash";
+            this.currenthashDataGridViewTextBoxColumn.HeaderText = "currenthash";
+            this.currenthashDataGridViewTextBoxColumn.Name = "currenthashDataGridViewTextBoxColumn";
+            // 
+            // previoushashDataGridViewTextBoxColumn
+            // 
+            this.previoushashDataGridViewTextBoxColumn.DataPropertyName = "previoushash";
+            this.previoushashDataGridViewTextBoxColumn.HeaderText = "previoushash";
+            this.previoushashDataGridViewTextBoxColumn.Name = "previoushashDataGridViewTextBoxColumn";
+            // 
+            // blockcountDataGridViewTextBoxColumn
+            // 
+            this.blockcountDataGridViewTextBoxColumn.DataPropertyName = "blockcount";
+            this.blockcountDataGridViewTextBoxColumn.HeaderText = "blockcount";
+            this.blockcountDataGridViewTextBoxColumn.Name = "blockcountDataGridViewTextBoxColumn";
+            // 
+            // dbBlockchainBindingSource
+            // 
+            this.dbBlockchainBindingSource.DataMember = "dbBlockchain";
+            this.dbBlockchainBindingSource.DataSource = this.dbBlockchainDataSet1;
+            // 
+            // dbBlockchainDataSet1
+            // 
+            this.dbBlockchainDataSet1.DataSetName = "dbBlockchainDataSet1";
+            this.dbBlockchainDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dbBlockchainTableAdapter
+            // 
+            this.dbBlockchainTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 721);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBlockcount);
+            this.Controls.Add(this.txtBlockchain);
+            this.Controls.Add(this.btnNewblock);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTransaction);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbBlockchainBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbBlockchainDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTransaction;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNewblock;
+        private System.Windows.Forms.TextBox txtBlockchain;
+        private System.Windows.Forms.TextBox txtBlockcount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private dbBlockchainDataSet1 dbBlockchainDataSet1;
+        private System.Windows.Forms.BindingSource dbBlockchainBindingSource;
+        private dbBlockchainDataSet1TableAdapters.dbBlockchainTableAdapter dbBlockchainTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn transcationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn transcationhashDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn currenthashDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn previoushashDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn blockcountDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Blockchain/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Blockchain
+{
+    public partial class Form1 : Form
+    {
+        //Variables
+        List<float[,,,]> Blockchain = new List<float[,,,]>();
+        string PreviousHash = "0";
+        string CurrentHash;
+        string Transaktion;
+        string TransaktionHash;
+        string GenesisBlockHash = "c423b0dcb3697b21d846dbf01f6f2438fbc973078bd8fc671551f5b98e8af0f0";
+        int BlockCount = 0;
+
+        SqlConnection connection;
+        string connectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["Blockchain.Properties.Settings.dbBlockchainConnectionString"].ConnectionString;
+        }
+
+        /// <summary>
+        /// Creates a new Block depends on Prevoius BlockHash and TxHash
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNewblock_Click(object sender, EventArgs e)
+        {
+
+            //GenesisBlock generate block
+            if (BlockCount == 0)
+            {
+                Transaktion = "Das ist ein Test";   //First TX
+                BlockCount = 0;                    //No Previous Block          //GenesisBlock = 0
+                CurrentHash = ComputeSha256Hash(Transaktion);
+                TransaktionHash = CurrentHash;
+
+                //Check if Hardcoded is equal Block 0 at new Start.
+                if (GenesisBlockHash != CurrentHash)
+                {
+                    MessageBox.Show("Wrong GenesisBlockHash. The Hardcoded hash is " + GenesisBlockHash + " and the new generated is " + CurrentHash);
+                    return;
+                }
+            }
+
+            // Generate new Block (non GenesisBlock)
+            else if (BlockCount != 0)
+            {
+                Transaktion = txtTransaction.Text;
+                TransaktionHash = ComputeSha256Hash(Transaktion);
+                var NewHash = TransaktionHash + PreviousHash;
+                CurrentHash = ComputeSha256Hash(NewHash);
+                //Hash is to short/long
+                if (CurrentHash.Length != 64)
+                {
+                    MessageBox.Show("Block: " + BlockCount + " is corupted. Wrong Hash: " + CurrentHash);
+                }
+            }
+
+            txtBlockcount.Text = Convert.ToString(BlockCount);
+            txtBlockchain.Text += CurrentHash.ToString() + "\r\n";
+            write_db();
+            this.dbBlockchainTableAdapter.Fill(this.dbBlockchainDataSet1.dbBlockchain);
+            BlockCount++;
+            PreviousHash = CurrentHash;
+
+        }
+
+        string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Diese Codezeile lädt Daten in die Tabelle "dbBlockchainDataSet1.dbBlockchain". Sie können sie bei Bedarf verschieben oder entfernen.
+            this.dbBlockchainTableAdapter.Fill(this.dbBlockchainDataSet1.dbBlockchain);
+            //Load db in GridViewList
+            //get_db();
+            //Get latest Id from db
+            getLatestID_db();
+            // MessageBox.Show(latestID);
+        }
+
+        /// <summary>
+        /// Writes the whole Block to the DB 
+        /// </summary>
+        private void write_db()
+        {
+            using (connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO dbBlockchain (transcation, transcationhash, currenthash, previoushash, blockcount) VALUES(@Transcation,@Transcationhash,@Currenthash,@Previoushash,@Blockcount)", connection))
+                {
+                    // create your parameters
+                    cmd.Parameters.Add("@Transcation", System.Data.SqlDbType.NVarChar);
+                    cmd.Parameters.Add("@Transcationhash", System.Data.SqlDbType.NVarChar);
+                    cmd.Parameters.Add("@Currenthash", System.Data.SqlDbType.NVarChar);
+                    cmd.Parameters.Add("@Previoushash", System.Data.SqlDbType.NVarChar);
+                    cmd.Parameters.Add("@Blockcount", System.Data.SqlDbType.NVarChar);
+
+                    //Cmd.Parameters["@CustomerID"].Value = Convert.ToInt32(textbox1.Text);
+                    cmd.Parameters["@Transcation"].Value = Transaktion;
+                    cmd.Parameters["@Transcationhash"].Value = TransaktionHash;
+                    cmd.Parameters["@Currenthash"].Value = CurrentHash;
+                    cmd.Parameters["@Previoushash"].Value = PreviousHash;
+                    cmd.Parameters["@Blockcount"].Value = Convert.ToString(BlockCount);
+                    int rows = cmd.ExecuteNonQuery();
+                    //MessageBox.Show("Row inserted !! ");
+                    //MessageBox.Show(Convert.ToString(rows));
+                    //get_db();
+                    connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Test function
+        /// Is for tests
+        /// </summary>
+        //private void get_db()
+        //{
+        //    using (connection = new SqlConnection(connectionString))
+        //    using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM dbBlockchain", connection))
+        //    {
+        //        DataTable serverTable = new DataTable();
+        //        adapter.Fill(serverTable);
+
+        //        lstExplorer.DisplayMember = "Currenthash";
+        //        lstExplorer.ValueMember = "Id";
+        //        lstExplorer.DataSource = serverTable;
+        //    }
+        //}
+
+
+        /// <summary>
+        /// Recives the latest ID from Blockchain to generate the following Block
+        /// Recives the latest hash from Blockchain to generate the following Block
+        /// </summary>
+        private void getLatestID_db()
+        {
+            using (connection = new SqlConnection(connectionString))
+            {
+                string query = "select top 1 * from dbBlockchain  order by ID desc";
+                var _test = new object[6];
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                var _latestID = (reader.GetValue(i));
+
+                                _test[i] = _latestID;
+                            }
+                        }
+                    }
+                    BlockCount = Convert.ToInt32(_test[5]) + 1;
+                    PreviousHash = Convert.ToString(_test[3]);    //Latest hash from db
+                    connection.Close();
+                }
+            }
+        }
+
+        //private void getLastHash_db()
+        //{
+        //    using (connection = new SqlConnection(connectionString))
+        //    using (SqlCommand cmd = new SqlCommand("SELECT * FROM dbBlockchain WHERE id = @latestID", connection))
+        //    {
+        //        connection.Open();
+        //        cmd.Parameters.Add("@latestID", System.Data.SqlDbType.NVarChar);
+        //        cmd.Parameters["@latestID"].Value = latestID;
+
+        //        using (SqlDataReader reader = cmd.ExecuteReader())
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                for (int i = 0; i < 1; i++)
+        //                {
+        //                    var _latestHash = (reader.GetValue(i));
+        //                    MessageBox.Show(Convert.ToString(_latestHash));
+        //                }
+        //            }
+        //        }
+
+        //        connection.Close();
+        //    }
+        //}
+
+    }
+}
